179a823c6b441ec414bc39518358aefd3625571f
imkhoat <khoatbui.idea@gmail.com>
2022-01-07 20:15:28 +0700

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="home-page">
+    <a-button type="primary">
+      <template #default>Delete</template>
+    </a-button>
+    <a-badge :count="9">
+      <a-avatar shape="square" />
+    </a-badge>
+  </main>
+</template>
+<style lang="less">
+.home-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
