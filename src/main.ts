07771bfc0208e9632c2d9e3a59c9083996c459ea
imkhoat <khoatbui.idea@gmail.com>
2022-01-08 23:35:36 +0700

    feat: add prettierc

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ArcoVue from '@arco-design/web-vue';
-import '@arco-design/web-vue/dist/arco.css';
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
 
 import App from './App.vue'
 import router from './router'
